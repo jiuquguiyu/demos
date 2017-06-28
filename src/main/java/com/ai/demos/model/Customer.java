de8d44484234474fc4948c3f959f428c0e2d621e
jiuquguiyu <794712920@qq.com>
2017-06-28 14:41:34 +0800

    add class

---
+++ src/main/java/com/ai/demos/model/Customer.java
@@ -0,0 +1,46 @@
+package com.ai.demos.model;
+
+public class Customer {
+
+	private String name;
+	
+	private String pwd;
+	
+	private String age;
+	
+	private String sex;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+	
+}
