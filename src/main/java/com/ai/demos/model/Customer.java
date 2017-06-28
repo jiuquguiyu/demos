30bfa168e64e944aceb5ea62e8bf427281b49998
tianhq <794712920@qq.com>
2017-06-28 14:44:28 +0800

    add customer class

---
+++ src/main/java/com/ai/demos/model/Customer.java
@@ -0,0 +1,26 @@
+package com.ai.demos.model;
+
+public class Customer {
+
+	private String username;
+	
+	private String student;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getStudent() {
+		return student;
+	}
+
+	public void setStudent(String student) {
+		this.student = student;
+	}
+	
+	
+}
